be5dad0c19fd6b75bd7c62033201b369250e0eb0
Bienvenu Faraja <shawnbienvenu@gmail.com>
2021-10-13 17:20:47 +0300

    2.2 commit to gh pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,27 @@
 function Footer() {
 	return (
-		<footer className='footer'>
-			<div className='links'>
-				<h4>Socials</h4>
-				<div>
-					<a
-						href='https://www.linkedin.com/in/bienvenu-faraja/'
-						target='_blank'
-						rel='noreferrer'>
-						LinkedIn
-					</a>
-					<a
-						href='https://twitter.com/farajabien'
-						target='_blank'
-						rel='noreferrer'>
-						Twitter
-					</a>
+		<footer>
+			<div className='footer'>
+				<div className='links'>
+					<h4>Socials</h4>
+					<div>
+						<a
+							href='https://www.linkedin.com/in/bienvenu-faraja/'
+							target='_blank'
+							rel='noreferrer'>
+							LinkedIn
+						</a>
+						<a
+							href='https://twitter.com/farajabien'
+							target='_blank'
+							rel='noreferrer'>
+							Twitter
+						</a>
+					</div>
 				</div>
-			</div>
-			<div>
-				<p>Copyright &copy; 2020 - Diego</p>
+				<div className='copyright'>
+					<p>Copyright &copy; 2020 - Diego</p>
+				</div>
 			</div>
 		</footer>
 	)
